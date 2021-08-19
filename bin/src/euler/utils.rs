d5c5ad5555e8a19b197be29c30403a1740d417a0
Cristián Antuña <cristiansud@gmail.com>
2021-08-19 19:00:01 -0300

    Problema 2

--- bin/src/euler/utils.rs
+++ bin/src/euler/utils.rs
@@ -16,3 +16,27 @@ pub fn multiples_under(multiples : Vec<u32>, upto : u32) -> Vec<u32> {
 fn is_multiple(number : u32, base : u32) -> bool {
     number % base == 0
 }
+
+pub fn is_even(number : u32) -> bool {
+    is_multiple(number, 2)
+}
+
+pub struct FibonacciSequence {
+    last_two : [u32; 2],
+}
+
+impl FibonacciSequence {
+
+    pub fn new_fibonacci() -> FibonacciSequence {
+	FibonacciSequence{last_two : [0,1]}
+    }
+    
+    pub fn next(&mut self) -> u32 {
+	let next = self.last_two.iter().sum();
+
+	self.last_two[1] = self.last_two[0];
+	self.last_two[0] = next;
+	
+	next
+    }
+}
--- bin/src/main.rs
+++ bin/src/main.rs
@@ -5,5 +5,9 @@ pub use crate::euler::euler_problems;
 fn main() {
     let answer_1 = euler_problems::problem_1_solver(vec![3,5], 1000);
     println!("The answer to problem 1 is: {}", answer_1);
+
+    let answer_2 = euler_problems::problem_2_solver(4000000);
+    println!("The answer to problem 2 is: {}", answer_2);
+
 }
 
