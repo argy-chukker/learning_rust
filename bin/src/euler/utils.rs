edac2bee0c5e6e9ed85e13581ad086d28b5fb98b
Cristián Antuña <cristiansud@gmail.com>
2021-08-20 18:38:20 -0300

    Problem 3

--- bin/src/euler/utils.rs
+++ bin/src/euler/utils.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 pub fn multiples_under(multiples : Vec<u32>, upto : u32) -> Vec<u32> {
 
     let mut answer : Vec<u32> = vec![];
@@ -39,4 +41,42 @@ impl FibonacciSequence {
 	
 	next
     }
+}
+
+pub fn primes_below(n : u32) -> Vec<u32> {
+    let top = (n as f64).sqrt() as usize + 1;
+    
+    let mut candidates : Vec<u32> = (2..n+1).collect();
+
+    for i in 2..top {
+	if candidates[i-2] > 0 {
+	    let max_j = n as usize / i +1 ;
+	    for j in i..max_j {
+	 	candidates[i*j-2] = 0;
+	     }
+	 }
+    }
+
+    let mut result : Vec<u32> = Vec::new();
+    for c in candidates {
+	if c > 0 {result.push(c)}
+    }
+    result
+}
+
+pub fn factorize(n : u32) -> Vec<u32> {
+    let candidates = primes_below(n);
+    let mut result : Vec<u32> = Vec::new();
+
+    let mut rest = n;
+    
+    for c in &candidates {
+	if rest % c == 0
+	{
+	    result.push(*c);
+	    while rest % c == 0 {rest /= c;};
+	};
+    };
+    if result == [] {result.push(rest);};
+    result
 }
--- bin/src/main.rs
+++ bin/src/main.rs
@@ -3,11 +3,19 @@ pub mod euler;
 pub use crate::euler::euler_problems;
 
 fn main() {
+    report_euler_answers();
+}
+
+fn report_euler_answers() -> () {
     let answer_1 = euler_problems::problem_1_solver(vec![3,5], 1000);
     println!("The answer to problem 1 is: {}", answer_1);
 
     let answer_2 = euler_problems::problem_2_solver(4000000);
-    println!("The answer to problem 2 is: {}", answer_2);
+    println!("The answer to problem 2 is: {}", answer_2);    
 
+    // By brute force I know 1471 is a prime divisor.
+    let answer_3 = euler_problems::problem_3_solver((600851475143_f64 / 1471.0) as u32);
+    println!("The answer to problem 3 is: {:?}", answer_3) ;    
 }
-
+ 
+ 
